25bb82cdd641912e8d5526346de7771d42151740
kengolgol <79845967+kengolgol@users.noreply.github.com>
2021-05-10 14:28:11 +0900

    Issue4

--- test/controllers/plans_controller_test.rb
+++ test/controllers/plans_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CalendarsControllerTest < ActionDispatch::IntegrationTest
+class PlansControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
